d9170d54fdc0168beef8fef31f41d5c39d3e83e2
faturrachman01 <211220115@unmuhpnk.ac.id>
2024-08-05 08:27:52 +0700

    ,

--- app/Models/stockmovement.php
+++ app/Models/stockmovement.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class stockmovement extends Model
+class StockMovement extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['item_id', 'warehouse_id', 'quantity', 'movement_type'];
 }
